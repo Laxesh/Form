05b4f1b036a45b0fa853e95d59f1b66847e3721a
Laxesh <laxeshhirapara@gmail.com>
2024-09-10 19:32:18 +0530

    .

--- js/name.js
+++ js/name.js
@@ -2,7 +2,7 @@ const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const names = [];
 const buttonsState = {};
 
-function createbutton() {
+function createButton() {
   const buttonsContainer = document.getElementById("buttonsContainer");
   for (const char in alphabet) {
     const button = document.createElement("button");
@@ -10,28 +10,13 @@ function createbutton() {
     buttonsContainer.appendChild(button);
     button.id = `btn-${char}`;
     button.style = "margin  : 10px";
-    button.addEventListener("click", () => handleAlphabetButtonClick(char));
   }
 }
-createbutton();
+createButton();
 
 function submitName() {
   const name = document.getElementById("name").value;
 
   names.push(name);
   localStorage.setItem("names", JSON.stringify(names));
-}
-
-function handleAlphabetButtonClick(char) {
-  const button = document.getElementById(`btn-${char}`);
-  const isActive = button.classList.contains("highlight");
-
-  // Toggle button state
-  if (isActive) {
-    button.classList.remove("highlight");
-    buttonsState[char] = false;
-  } else {
-    button.classList.add("highlight");
-    buttonsState[char] = true;
-  }
 }
