dedefa0b28be32a59a844144c1a85d6a878186b3
Jatin Undhad <jatinundhad33@gmail.com>
2024-09-11 20:58:22 +0530

    jatin: added a logic

--- js/name.js
+++ js/name.js
@@ -2,20 +2,35 @@ const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const names = [];
 const buttonsState = {};
 
+const namesStartWithFirstCharacters = {};
+
 function createButton() {
   const buttonsContainer = document.getElementById("buttonsContainer");
   for (const char in alphabet) {
     const button = document.createElement("button");
     button.textContent = alphabet[char];
     buttonsContainer.appendChild(button);
-    button.id = `btn-${char}`;
+    button.id = `btn-${alphabet[char]}`;
     button.style = "margin  : 10px";
   }
 }
+
 createButton();
 
 function submitName() {
   const name = document.getElementById("name").value;
+
+  if(!name.trim()) throw new Error("Please enter a name!!!");
+
+  const firstCharacter = name[0]; // getting the first character of the name
+
+  if(!(firstCharacter in namesStartWithFirstCharacters)) {
+    namesStartWithFirstCharacters[firstCharacter] = [name];
+  }else{
+    namesStartWithFirstCharacters[firstCharacter].push(name);
+  }
+
+  document.getElementById(`btn-${alphabet[char]}`).innerText = `${firstCharacter} (${namesStartWithFirstCharacters[firstCharacter].length})`;
 
   names.push(name);
   localStorage.setItem("names", JSON.stringify(names));
