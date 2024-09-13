9ef86fbb2ee51ea15e63d4e61c5b8c2c8baf51b8
Laxesh <laxeshhirapara@gmail.com>
2024-09-13 12:00:37 +0530

    ./

--- js/name.js
+++ js/name.js
@@ -8,21 +8,42 @@ function createButton() {
     const button = document.createElement("button");
     button.textContent = alphabet[char];
     buttonsContainer.appendChild(button);
+    button.addEventListener("click", () => {
+      clickButton(char);
+    });
     button.id = `btn-${alphabet[char]}`;
     button.style = "margin  : 10px";
     buttonsState[char] = false;
   }
-}
 
+  const allBtn = document.createElement("button");
+  allBtn.textContent = "All";
+  buttonsContainer.appendChild(allBtn);
+  allBtn.id = "btn-all";
+  allBtn.style = "margin  : 10px";
+}
 createButton();
 
 function submitName() {
   const inputName = document.getElementById("name").value.trim();
-
   if (inputName) {
     names.push(inputName);
+    inputName.value = " ";
   }
-
   localStorage.setItem("names", JSON.stringify(names));
-  inputName.value = "";
+}
+
+function handleAllButtonClick() {
+  const allButtons = document.querySelectorAll("#buttonsContainer button");
+}
+
+function clickButton(char) {
+  const button = document.getElementById(`btn-${char}`);
+  const results = document.getElementById("results");
+
+  names.forEach((name) => {
+    if (name.includes(char)) {
+      results.innerHTML += `<p>${name}</p>`;
+    }
+  });
 }
